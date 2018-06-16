167d253d638c22f9b50ad183a8b71ee444981703
Hyomin-Lee <34526003+hyomin-Lee@users.noreply.github.com>
2018-06-16 23:24:21 +0900

    Add files via upload
    success OCR!

---
+++ hyo_pa_go/hyo_pa_go/src/hyomin/GoogleVisionApiTester.java
@@ -0,0 +1,72 @@
+package hyomin;
+
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.Image;
+import com.google.protobuf.ByteString;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class GoogleVisionApiTester {
+	
+	public static void main(String[] args) {
+		
+		index get_index = new index();
+		get_index.print_index();
+
+		get_OCR();
+	}
+	
+	
+	
+	static void get_OCR() {
+		Scanner scan = new Scanner(System.in);
+		String imageFilePath = scan.nextLine();
+		try {
+			System.err.close();
+		    System.setErr(System.out);
+			
+			
+			List<AnnotateImageRequest> requests = new ArrayList<>();
+		
+			ByteString imgBytes = ByteString.readFrom(new FileInputStream(imageFilePath));
+		
+			Image img = Image.newBuilder().setContent(imgBytes).build();
+			Feature feat = Feature.newBuilder().setType(Type.TEXT_DETECTION).build();
+			AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+			requests.add(request);
+		
+			try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+				BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+			    List<AnnotateImageResponse> responses = response.getResponsesList();
+		
+			    for (AnnotateImageResponse res : responses) {
+			    	if (res.hasError()) {
+			    		System.out.printf("Error: %s\n", res.getError().getMessage());
+			    		return;
+			    	}
+		
+			    	System.out.print("[*] String in image file : ");
+			    	System.out.println(res.getTextAnnotationsList().get(0).getDescription());
+			      
+			    	// For full list of available annotations, see http://g.co/cloud/vision/docs
+			    	/*for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+				    	  
+						//System.out.printf("Text: %s\n", annotation.getDescription());
+						//System.out.printf("Position : %s\n", annotation.getBoundingPoly());
+					}*/
+			    }
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ hyo_pa_go/hyo_pa_go/src/hyomin/index.java
@@ -0,0 +1,10 @@
+package hyomin;
+
+public class index {
+	void print_index(){
+		System.out.print("================================================");
+		System.out.print("\n\n\n\n  HYO PA GO ( hyomin translater ) - 20218 ÀÌÈ¿¹Î\n\n\n\n");
+		System.out.print("================================================\n");
+		System.out.print("[*]Input imageFile PATH >>> ");
+	}
+}
